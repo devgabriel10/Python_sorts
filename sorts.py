68684c73e336e03998cc785b570a723271ce7a57
Gabriel D'Andréa <webgabrielaraujo@gmail.com>
2024-10-22 12:02:22 -0500

    implements Bubble Sort

--- sorts.py
+++ sorts.py
@@ -11,3 +11,10 @@ def selection_sort(list):
             aux = list[j]
             list[j] = list[min_index]
             list[min_index] = aux
+
+def bubble_sort(list):
+    n = len(list)
+    for j in range(n-1):
+        for i in range(n-1):
+            if list[i] > list[i+1]:
+                list[i], list[i+1] = list[i+1], list[i]
--- testSorts.py
+++ testSorts.py
@@ -1,5 +1,5 @@
 import random
-from sorts import selection_sort
+from sorts import selection_sort, bubble_sort
 
 
 any_numbers = random.sample(range(1, 1000), 42)
@@ -7,6 +7,6 @@ any_numbers = random.sample(range(1, 1000), 42)
 if __name__ == "__main__":
     list = any_numbers
     print(list)
-    selection_sort(list)
+    bubble_sort(list)
     print("\n Ordered List:")
     print(list)
